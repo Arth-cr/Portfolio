3b425189ca7fc11da3fbf4741620ce3f4fe121b6
Arthur Ribeiro <arthur.ribeiro@agenciaecan.com.br>
2022-09-27 18:35:28 -0300

    feat: try 6 fix deploy error

--- pages/api/getPortugues.ts
+++ pages/api/getPortugues.ts
@@ -1,25 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import { groq } from "next-sanity"
-import { sanityClient } from "../../sanity"
-import { Portugues } from "../../typings"
-
-const query = groq`
-    *[_type == "pt"][0] {
-      ...,
-      experiences[] -> {..., technologies[] ->},
-      pageInfos ->,
-      projects[] -> {..., technologies[] ->}
-    }
-`
 
 type Data = {
-  portugues: Portugues
+  name: string
 }
 
-export default async function handler(
+export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const portugues: Portugues = await sanityClient.fetch(query)
-  res.status(200).json({ portugues })
+  res.status(200).json({ name: "John Doe" })
 }
