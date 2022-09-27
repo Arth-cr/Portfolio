fb6b075d976b3afc7fda096d442df081d20326e4
Arthur Ribeiro <arthur.ribeiro@agenciaecan.com.br>
2022-09-27 18:11:28 -0300

    feat: try 2 fix deplot error

--- sanity.ts
+++ sanity.ts
@@ -2,10 +2,10 @@ import { createClient } from "next-sanity"
 import createImageUrlBuilder from "@sanity/image-url"
 
 export const config = {
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: "production" || "production",
+  projectId: "hk2frivp"!,
   apiVersion: "2022-03-25",
-  useCdn: process.env.NODE_ENV !== "production",
+  useCdn: "production" !== "production",
 }
 
 export const sanityClient = createClient(config)
--- utils/fetchEnglish.ts
+++ utils/fetchEnglish.ts
@@ -1,7 +1,7 @@
 import { English } from "../typings"
 
 export const fetchEnglish = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getEnglish`)
+  const res = await fetch(`http://localhost:3000/api/getEnglish`)
 
   const data = await res.json()
   const en: English = data.en
