669e2635ac60399175fb8113cca7266cd9578176
Arthur Ribeiro <arthur.ribeiro@agenciaecan.com.br>
2022-09-27 18:52:14 -0300

    feat: try 8 fix deploy error

--- utils/fetchEnglish.ts
+++ utils/fetchEnglish.ts
@@ -4,7 +4,8 @@ export const fetchEnglish = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getEnglish`)
 
   const data = await res.json()
-  const en: English = data.en
+
+  const en: English = data.english
 
   return en
 }
