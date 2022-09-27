b5cea69404effb9ea315fb79569090365c0416a2
Arthur Ribeiro <arthur.ribeiro@agenciaecan.com.br>
2022-09-27 18:20:11 -0300

    feat: try 4 fix deploy error

--- utils/fetchPortugues.ts
+++ utils/fetchPortugues.ts
@@ -1,7 +1,9 @@
 import { Portugues } from "../typings"
 
 export const fetchPortugues = async () => {
-  const res = await fetch(`http://localhost:3000/api/getPortugues`)
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPortugues`
+  )
 
   const data = await res.json()
   const pt: Portugues = data.pt
